067c097fd430f9f29ef03d27d8e4b6f1b8e18e4c
Beasley <beaslera83@gmail.com>
2018-07-20 14:21:26 -0500

    Initial commit

---
+++ src/app/Models/memberModel.ts
@@ -0,0 +1,6 @@
+export class memberModel {
+    public name: string;
+    public image: string;
+    public points: number;
+    public respawn: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MatCardModule, MatDividerModule} from '@angular/material/'
+import { memberModel } from './Models/memberModel';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Nerf War';
+  players: memberModel[] = [{
+    name: "player1",
+    image: "string",
+    points: 0,
+    respawn: 0
+  },{
+    name: "player2",
+    image: "string",
+    points: 0,
+    respawn: 0
+  },{
+    name: "player3",
+    image: "string",
+    points: 0,
+    respawn: 0
+  },{
+    name: "player4",
+    image: "string",
+    points: 0,
+    respawn: 0
+  },{
+    name: "player5",
+    image: "string",
+    points: 0,
+    respawn: 0
+  }]
+
+
 }
+
